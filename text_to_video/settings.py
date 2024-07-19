82f6f56f67b1aa11140cf97945e4fe171b7ac1c7
Эмин <emitka90@yandex.ru>
2024-07-19 08:47:24 +0300

    Commit

--- text_to_video/settings.py
+++ text_to_video/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%7ymqt_tvx=*z9ug=$r4_^5gnfeq3q*=w3c885^d+c*(!d!*-r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['colab.research.google.com']
 
 
 # Application definition
